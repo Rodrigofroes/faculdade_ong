965a5ae29b79ee9c78520bdc9d21c46098c600ae
Rodrigo oliveira <craftland262@gmail.com>
2023-10-06 13:32:31 -0300

    faculdade_ong

---
+++ main.js
@@ -0,0 +1,16 @@
+function clickbutton()
+{
+  window.location.href = "index-login.html";
+}
+
+function clicklogin()
+{
+  window.location.href = "index-home.html";
+}
+
+function home(){
+  window.location.href = "index-user.html"
+}
+
+
+
